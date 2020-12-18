dfa12f5a3bdefbf29e3dc2b3a694d08af6903ba7
Ryan Conley <ryandeanconley2@yahoo.com>
2020-12-18 00:48:07 -0600

    final correction

--- src/bookmark.js
+++ src/bookmark.js
@@ -12,7 +12,7 @@ function getExpand() {
         foundMark.bookmark.expanded = !foundMark.bookmark.expanded;
         render.renderStoreBookmarks();
     })
-}
+} 
 
 function getCollapse() {
     $('.savedBookmarks').on('click keypress', '.expanded', function (event) {
@@ -25,7 +25,45 @@ function getCollapse() {
     })
 }
 
+function deleteButton() {
+    $('.savedBookmarks').on('click', '.delete', e => {
+        const position = $(e.currentTarget).closest('.border').find('.expanded').data('bookmark-id');
+        fetch(`${store.BASE_URL}/${position}`, {
+            method: 'DELETE'
+        }).then(response => {
+            if (!response.ok) {
+                throw response
+            }
+            for (let i = 0; i < store.store.length; i++) {
+                if (store.store[i].bookmark.id === position) {
+                    store.store.splice(i, 1);
+                    
+                }
+            }
+            console.log('rerendering map')
+            render.renderStoreBookmarks();
+        }).catch(err => {
+            console.log('Failed to submit')
+            $('.failedCreation').append(
+                $(`<p class="border">Failed to delete data from server!</p>`)
+            )
+            console.log(err)
+        });
+    })
+} 
+
+function filterChanged() {
+    $('document').ready(function () {
+        $('#filter').change(function () {
+            render.renderStoreBookmarks();
+        })
+    })
+}
+
+
 export default {
     getExpand,
-    getCollapse
+    getCollapse,
+    deleteButton,
+    filterChanged
 }
